bbdd8bab1b071e181cadba25f0e8512ea80bc750
Miguel Sandoval <miguelship3@gmail.com>
2020-09-10 07:40:22 +0200

    Added BurgerIngredient, LayoutCSSModule, BurgerIngred switch and porptype validation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const Layout = (props) => {
     return (
         <Aux>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            <main>
+            <main className={classes.Content}>
                 {props.children}
             </main>
         </Aux>
